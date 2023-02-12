abd3cd23bfc631ddb5708a1afdc37d724aaf2257
Etienne Pretorius <ettienne_pretorius@yahoo.co.uk>
2023-02-12 12:16:10 +0000

    - Removed unused dependency of ``aquarium-sea-turtle``. - Updated dependency of ``aquarium-octopus`` to v1.0.1. - Updated dependency of ``aquarium-triggerfish`` to v1.2.0. - Updated test dependency of ``aquarium-cmocka`` to v1.5.1.

--- cmake/FetchAquariumOctopus.cmake
+++ cmake/FetchAquariumOctopus.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         aquarium-octopus
         GIT_REPOSITORY https://github.com/pretore/aquarium-octopus.git
-        GIT_TAG v1.0.0
+        GIT_TAG v1.0.1
         GIT_SHALLOW 1
 )
 
